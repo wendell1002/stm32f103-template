32d1dd4338e072c8270881c40deae8accc187652
tietang <30461739@qq.com>
2025-08-02 18:54:07 +0800

    add panic

--- src/main.rs
+++ src/main.rs
@@ -36,8 +36,10 @@ fn main() -> ! {
     // or
     let mut delay = dp.TIM2.delay_us(&clocks);
     hprintln!("Hello, world!");
+{% if defmt -%}
     info!("Hello, world!");
     error!("Hello, world!");
+{% endif %}
     loop {
         // 设置高电平
         led.set_high();
